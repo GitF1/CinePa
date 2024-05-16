ca3c64c2382d77882aaf87f2d71d9d98b6ac617d
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2024-05-16 09:01:48 +0700

    Wrote template Review class

--- src/java/model/Review.java
+++ src/java/model/Review.java
@@ -4,10 +4,68 @@
  */
 package model;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author duyqu
  */
 public class Review {
+    private int userID;
+    private int movieID;
+    private int rating;
+    private LocalDateTime timeCreated;
+    private String content;
+
+    public Review() {
+    }
+
+    public Review(int userID, int movieID, int rating, LocalDateTime timeCreated, String content) {
+        this.userID = userID;
+        this.movieID = movieID;
+        this.rating = rating;
+        this.timeCreated = timeCreated;
+        this.content = content;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(int movieID) {
+        this.movieID = movieID;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public LocalDateTime getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalDateTime timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
     
 }
