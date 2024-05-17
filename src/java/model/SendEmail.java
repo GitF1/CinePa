d7286ecc6768005f4b7de02b8e2c3654346c8ecc
VINHNQ <VINHNQ@LAPTOP-FR3EODPJ>
2024-05-18 05:53:45 +0700

    Send mail and random, service xử lý các valid

---
+++ src/java/model/SendEmail.java
@@ -0,0 +1,75 @@
+package model;
+
+import com.sun.mail.smtp.SMTPSendFailedException;
+import java.util.Properties;
+import java.util.Random;
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+/**
+ * Sends an email to the user.
+ */
+public class SendEmail {
+
+
+    public SendEmail() {
+    }
+
+    public String getRanDom() {
+        Random rnd = new Random();
+        int number = rnd.nextInt(999999);
+        return String.format("%06d", number);
+    }
+
+    public boolean sendEmail(User user) {
+        boolean test = false;
+
+        String toEmail = user.getEmail();
+        String fromEmail = "vinhnqde170404@gmail.com";  // your email
+        String password = "nghwdeyzqrqgcyki";  // your app password
+
+        try {
+            Properties pr = new Properties();
+            pr.setProperty("mail.smtp.host", "smtp.gmail.com");
+            pr.setProperty("mail.smtp.port", "587");
+            pr.setProperty("mail.smtp.auth", "true");
+            pr.setProperty("mail.smtp.starttls.enable", "true");
+          
+
+            // SSL properties should not be used when using STARTTLS
+            pr.remove("mail.smtp.ssl.enable");
+            pr.remove("mail.smtp.socketFactory.port");
+            pr.remove("mail.smtp.socketFactory.class");
+            // Get session
+            Session session = Session.getInstance(pr, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(fromEmail, password);
+                }
+            });
+
+            Message mess = new MimeMessage(session);
+            mess.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+            mess.setFrom(new InternetAddress(fromEmail));
+            mess.setSubject("User Email Verification");
+            mess.setText("Registered successfully. Please verify your account using this code " + user.getCode());
+
+            Transport.send(mess);
+            test = true;
+
+        } catch (SMTPSendFailedException e) {
+            e.printStackTrace();  // Specific SMTP error
+        } catch (MessagingException e) {
+            e.printStackTrace();  // General messaging error
+        } catch (Exception e) {
+            e.printStackTrace();  // Any other exception
+        }
+        return test;
+    }
+}
---
+++ src/java/service/UserServiceImpl.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+import dal.UserDAOImpl;
+import java.util.List;
+import model.User;
+
+/**
+ *
+ * @author VINHNQ
+ */
+public class UserServiceImpl implements UserService {
+
+    UserDAOImpl userDAO = new UserDAOImpl();
+
+    @Override
+    public List<User> getAll() {
+        return userDAO.getAll();
+    }
+
+    @Override
+    public User findOne(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public User findOne(String username) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void insert(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean register(String username, String password, String email, String fullName, String code) {
+        if (userDAO.checkExistEmail(email)) {
+            return false;
+        }
+        if (userDAO.checkExistUsername(username)) {
+            return false;
+        }
+        userDAO.insertregister(new User(username, password, email, fullName, code, 0));
+        return true;
+    }
+
+    @Override
+    public boolean checkExistEmail(String email) {
+        return userDAO.checkExistEmail(email);
+    }
+    
+    @Override
+    public User login(String username, String password) {
+        User user = this.findOne(username); // Find the user by username
+        if (user != null && password.equals(user.getPassword())) {
+            return user; // Return the user if login is successful
+        }
+        return null; // Return null if login fails
+    }
+
+    @Override
+    public boolean checkExistUsername(String username) {
+        return userDAO.checkExistUsername(username);
+    }
+
+    @Override
+    public void updatestatus(User user) {
+        userDAO.updatestatus(user);
+    }
+
+}
