af8b6aba6f1a2d5d5634270a0842b61ef58301d6
VINHNQ <VINHNQ@LAPTOP-FR3EODPJ>
2024-05-19 15:31:20 +0700

    git cache cleared

--- src/java/DAO/UserDAOImpl.java
+++ src/java/DAO/UserDAOImpl.java
@@ -5,6 +5,7 @@
 package DAO;
 
 import DB.DBContext;
+import jakarta.servlet.ServletContext;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,7 +19,9 @@ import model.User;
  */
 public class UserDAOImpl extends DBContext {
 
-    public List<User> getAll() {
+ 
+
+  public List<User> getAll() {
         List<User> list = new ArrayList<>();
         String sql = "SELECT * FROM [User]";
         try {
@@ -34,45 +37,7 @@ public class UserDAOImpl extends DBContext {
         }
         return list;
     }
-
-    public void insert(User c) {
-        String sql = "INSERT INTO [dbo].[User]\n"
-                + " ([Username]\n"
-                + " ,[Password]\n"
-                + " ,[Email])\n"
-                + " ,[Code])\n"
-                + " VALUES (?,?,?,?)";
-        try {
-            PreparedStatement st = connection.prepareStatement(sql);
-            st.setString(1, c.getUsername());
-            st.setString(2, c.getPassword());
-            st.setString(3, c.getEmail());
-            st.setString(3, c.getCode());
-
-            st.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-    }
-
-    public User getUsertByID(String userName) {
-
-        String sql = "select*from dbo.[User] where userName=?";
-        try {
-            PreparedStatement st = connection.prepareStatement(sql);
-            st.setString(1, userName);
-            ResultSet rs = st.executeQuery();
-
-            if (rs.next()) {
-                User c = new User(rs.getString("fullName"), rs.getString("username"), rs.getString("password"), rs.getString("email"));
-                return c;
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return null;
-    }
-
+   
     public void updatestatus(User user) {
         String sql = "UPDATE [User] SET status = ?, code = ? WHERE email = ?";
 
@@ -89,43 +54,6 @@ public class UserDAOImpl extends DBContext {
             // Properly handle the exception, log it, or rethrow as a custom exception
             e.printStackTrace(); // Replace with appropriate logging in production code
         }
-    }
-
-    public User findOne(String username) {
-        String sql = "SELECT * FROM [User] WHERE Username = ?";
-        try {
-            PreparedStatement st = connection.prepareStatement(sql);
-            st.setString(1, username);
-            ResultSet rs = st.executeQuery();
-            if (rs.next()) {
-                User c = new User(
-                        rs.getInt("UserID"),
-                        rs.getString("AvatarLink"),
-                        rs.getString("Role"),
-                        rs.getString("Username"),
-                        rs.getString("Password"),
-                        rs.getString("Bio"),
-                        rs.getString("Email"),
-                        rs.getString("Fullname"), // Lấy giá trị của cột Fullname
-                        rs.getDate("Birthday"),
-                        rs.getString("Address"),
-                        rs.getBoolean("IsBanned"),
-                        rs.getInt("LevelPremiumID"),
-                        rs.getDouble("AccountBalance"),
-                        rs.getInt("BonusPoint"),
-                        rs.getString("Province"),
-                        rs.getString("District"),
-                        rs.getString("Commune"),
-                        rs.getString("Code"),
-                        rs.getInt("Status")
-                );
-
-                return c;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     public boolean checkExistUsername(String username) {
@@ -200,5 +128,7 @@ public class UserDAOImpl extends DBContext {
             e.printStackTrace(); // Replace with appropriate logging in production code
         }
     }
+    
+    
 
 }
---
+++ src/java/DB/SQLServerConnect.java
@@ -0,0 +1,67 @@
+//package DB;
+//
+//import jakarta.servlet.ServletContext;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.sql.Connection;
+//import java.sql.DatabaseMetaData;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//import java.util.Properties;
+//
+//public class SQLServerConnect {
+//
+//    protected Connection connection;
+//
+//    public SQLServerConnect() {
+//    }
+//
+//    public void closeConnection() {
+//        try {
+//            if (this.connection != null) {
+//                connection.close();
+//            }
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+//
+//    public Connection connect(ServletContext context) throws Exception {
+//
+//        Properties props = new Properties();
+//        try (FileInputStream fis = new FileInputStream(context.getRealPath("/WEB-INF/config/private/dbconfig.properties"))) {
+//            props.load(fis);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            throw new Exception("Error loading database configuration", e);
+//        }
+//
+//        String serverName = props.getProperty("db.serverName");
+//        String databaseName = props.getProperty("db.databaseName");
+//        String username = props.getProperty("db.username");
+//        String password = props.getProperty("db.password");
+//
+//        try {
+//            String URLConnect = "jdbc:sqlserver://" + serverName + ";databaseName=" + databaseName + ";user=" + username + ";password=" + password + ";encrypt=false";
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            connection = DriverManager.getConnection(URLConnect);
+//
+//            if (connection == null) {
+//                throw new Exception("Error When Connecting");
+//            }
+//
+//            System.out.println("Connected");
+//            DatabaseMetaData dm = (DatabaseMetaData) connection.getMetaData();
+//
+//            return connection;
+//
+//        } catch (SQLException e) {
+//            System.err.println("Cannot connect database, " + e);
+//        }
+//        return null;
+//    }
+//
+//    public static void main(String[] args) {
+//     
+//    }
+//}
