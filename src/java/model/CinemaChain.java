e8bc59f73c1c1b4d63c8eb28f06c6ceb8e6d0430
KyAnh1126 <anh261104@gmail.com>
2024-06-02 15:22:32 +0700

    Create CinemaChain.java class

---
+++ src/java/model/CinemaChain.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ACER
+ */
+public class CinemaChain {
+    private int cinemaChainID;
+    private String name;
+    private String information;
+    private String avatar;
+
+    public CinemaChain(int cinemaChainID, String name, String information, String avatar) {
+        this.cinemaChainID = cinemaChainID;
+        this.name = name;
+        this.information = information;
+        this.avatar = avatar;
+    }
+
+    public int getCinemaChainID() {
+        return cinemaChainID;
+    }
+
+    public void setCinemaChainID(int cinemaChainID) {
+        this.cinemaChainID = cinemaChainID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Override
+    public String toString() {
+        return "Cinema{" + "cinemaChainID=" + cinemaChainID + ", name=" + name + ", information=" + information + '}';
+    }
+}
