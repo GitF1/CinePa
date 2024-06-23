c1e73f5a447ec314383509db40243160e9305f6b
KyAnh1126 <anh261104@gmail.com>
2024-06-23 17:16:35 +0700

    Add FavoriteMovie and Review phim functions, changed HandleDisplayMovieInfo.java and DisplayMovieInfo.jsp. MovieDAO: deleted ReviewID from query review in db

--- src/java/controller/movie/HandleDisplayMovieInfo.java
+++ src/java/controller/movie/HandleDisplayMovieInfo.java
@@ -1,6 +1,9 @@
 package controller.movie;
 
+import DAO.FavouriteMoviesDAO;
 import DAO.MovieDAO;
+import DAO.ReviewMovieDAO;
+import DAO.UserDAO;
 import jakarta.servlet.ServletContext;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
@@ -14,8 +17,14 @@ import java.util.logging.Logger;
 import model.movie.MovieInfo;
 import model.Review;
 import DAO.schedule.ScheduleDAO;
+import jakarta.servlet.http.HttpSession;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import model.User;
 import model.schedule.CinemaMovieSlot;
 import util.RouterJSP;
 
@@ -24,6 +33,9 @@ public class HandleDisplayMovieInfo extends HttpServlet {
 
     ScheduleDAO scheduleDAO;
     MovieDAO movieDAO;
+    UserDAO userDAO;
+    ReviewMovieDAO reviewMovieDAO;
+    FavouriteMoviesDAO favoriteMoviesDAO;
     RouterJSP route = new RouterJSP();
 
     @Override
@@ -41,9 +53,8 @@ public class HandleDisplayMovieInfo extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         try {
-
+            userDAO = new UserDAO(request.getServletContext());
             // nhan movieID tu Quang Vinh guii qua :
             String movieID = request.getParameter("movieID");
             if (movieID == null) {
@@ -68,15 +79,40 @@ public class HandleDisplayMovieInfo extends HttpServlet {
             ArrayList<Review> listReviews = new ArrayList<>();
 
             listReviews = movieDAO.getReviewsByMovieID(Integer.parseInt(movieID), context);
-            //
 //            List<CinemaMovieSlot> listCinemaMovieSlots = scheduleDAO.getCinemasShowingMovie(Integer.parseInt(movieID), null);
 //
 //            System.out.println("list cinema movie slot:" + listCinemaMovieSlots);
 
+            
+            Map<Review, User> userReviews = new HashMap<>();
+
+            System.out.println(userReviews);
+            
+            reviewMovieDAO = new ReviewMovieDAO(request.getServletContext());
+            favoriteMoviesDAO = new FavouriteMoviesDAO(request.getServletContext());
+            HttpSession session = request.getSession();
+            System.out.println(session.getAttribute("userID"));
+            int userID = -1;
+            if(session.getAttribute("userID") != null) userID = (int)session.getAttribute("userID");
+            Boolean canReview = false;
+            Boolean isFavoritedMovie = null;
+            if(userID != -1) {
+                canReview = reviewMovieDAO.canReview(userID, Integer.parseInt(movieID));
+                isFavoritedMovie = favoriteMoviesDAO.isFavoritedMovie(userID, Integer.parseInt(movieID));
+            }
+                        
+            for(Review review : listReviews) {
+                User user = userDAO.getUserById(review.getUserID());
+                userReviews.put(review, user);
+            }
+            
             // them vao Request :
             request.setAttribute("movie", movie);
             request.setAttribute("listAvailableMovies", listAvailabelMovies);
             request.setAttribute("listReviews", listReviews);
+            request.setAttribute("userReviews", userReviews);
+            request.setAttribute("canReview", canReview);
+            request.setAttribute("isFavoritedMovie", isFavoritedMovie);
 
             request.getRequestDispatcher("page/movie/DisplayMovieInfo.jsp").forward(request, response);
 
@@ -91,14 +127,47 @@ public class HandleDisplayMovieInfo extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        int userID = (int)session.getAttribute("userID");
+        int movieID = Integer.parseInt(request.getParameter("movieID"));
+        
+        boolean isSendingFeedback = request.getParameter("isSendingFeedback") != null && request.getParameter("isSendingFeedback").equals("true");
+        boolean isAddingToFavorite = request.getParameter("isAddingToFavorite") != null && request.getParameter("isAddingToFavorite").equals("true");
+        
+        
+        
+        if(isAddingToFavorite) {
+            String favoritedAt = request.getParameter("favoritedAt");
+            try {
+                favoriteMoviesDAO = new FavouriteMoviesDAO(request.getServletContext());
+                favoriteMoviesDAO.insertFavouriteMovie(userID, movieID, favoritedAt);
+                doGet(request, response);
+            } catch (Exception ex) {
+                Logger.getLogger(HandleDisplayMovieInfo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return;
+        }
+        
+        if(isSendingFeedback) {
+            String reviewContent = request.getParameter("reviewText");
+            int rating = Integer.parseInt(request.getParameter("starOutput"));
+            String timeCreated = request.getParameter("timeCreatedOutput");
+            try {
+                reviewMovieDAO = new ReviewMovieDAO(request.getServletContext());
+                reviewMovieDAO.insertReview(userID, movieID, rating, timeCreated, reviewContent);
+                doGet(request, response);
+            } catch (Exception ex) {
+                Logger.getLogger(HandleDisplayMovieInfo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return;
+        }
 
         scheduleDAO.handleDoPostComponentSchedule(request, response);
 
         String redirectUrl = "/movie/HandleDisplayMovieInfo";
         response.setContentType("text/plain");
         response.getWriter().write(redirectUrl);
-
+        
         doGet(request, response);
     }
-
 }
---
+++ web/page/movie/reviewMovie/Test.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : Test
+    Created on : Jun 20, 2024, 2:28:45 PM
+    Author     : ACER
+--%>
+
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <div style="height: 200px;"></div>
+        <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+            Popover on top
+        </button>
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+                var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+                    return new bootstrap.Popover(popoverTriggerEl);
+                });
+            });
+        </script>
+    </body>
+</html>
